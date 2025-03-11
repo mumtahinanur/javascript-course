e939eb071ef9c965fa1aabf58b99f95ad6f56ed5
Mumtahina Nur <mumtahinanur9@gmail.com>
2025-03-11 22:07:51 +0600

    lesson 12

--- scripts/12-todo-list.js
+++ scripts/12-todo-list.js
@@ -11,8 +11,7 @@ renderTodoList();
 function renderTodoList() {
   let todoListHTML = '';
 
-  for(let i = 0; i < todoList.length; i++) {
-    const todoObject = todoList[i];
+  todoList.forEach(function(todoObject, index) {
     //const name = todoObject.name;
     //const dueDate = todoObject.dueDate;
     const {name, dueDate} = todoObject;
@@ -20,14 +19,14 @@ function renderTodoList() {
         <div>${name}</div> 
         <div>${dueDate}</div>
         <button onclick="
-          todoList.splice(${i}, 1);
+          todoList.splice(${index}, 1);
           renderTodoList();
           //Whenever we update the todo list, save in localStorage
           saveToStorage();
         " class="delete-todo-button">Delete</button>
       `;
     todoListHTML += html;
-  }
+  });
 
   document.querySelector('.js-todo-list').innerHTML = todoListHTML;
 }
